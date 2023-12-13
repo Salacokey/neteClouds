21ef6c00198c89d813ef67583b708908217f6fa7
Salacokey <2073399165@qq.com>
2023-12-13 18:34:08 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = defineConfig({
         org: "厚浦",
         project: "vue-sentry",
         // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
-        authToken: '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',
+        authToken: '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',
       }),
   ],
   }
